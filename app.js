f423fc882d28bf116c9f1dd05ff5c77eba04e5ba
hanchaewoong <hco2995@naver.com>
2023-10-05 12:59:44 +0900

    기본 레이아웃 완성

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+//view template engine
+app.set("view engine", "pug");
+app.set("views", __dirname+"/views");
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("home")
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact"); // help.pug 찾아서 서버에서 렌더링해라!
+})
+app.get("/portfolio", (req, res) => {
+    res.render("portfolio"); // help.pug 찾아서 서버에서 렌더링해라!
+})
+app.get("/about", (req, res) => {
+    res.render("about"); // help.pug 찾아서 서버에서 렌더링해라!
 })
 
 app.listen(port, () => {
