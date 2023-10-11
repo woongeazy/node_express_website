aaa1c664e7c01bb7b9d68d08ce01de85a69172df
hanchaewoong <hco2995@naver.com>
2023-10-11 12:17:44 +0900

    mysql 연결 및 DB입력 완성

--- app.js
+++ app.js
@@ -13,17 +13,19 @@ app.use(express.urlencoded({ extended: false }))
 
 // create the connection to database
 const conn = mysql.createConnection({
-  host: 'http://woong2.dothome.co.kr',
-  user: 'woong2',
-  database: 'Hanchae3629!'
+  host: 'localhost',
+  user: 'newuser',
+  password: "36298917",
+  database: 'contact',
+  dateStrings: "date"
 });
 
 // simple query
 conn.query(
-  'SELECT * FROM `contact`',
+  'SELECT * FROM contact.contacts',
   function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
+    console.log(results); // 서버로 부터 반환되는 결과 행
+    // console.log(fields); // 결과에 따른 메타 데이터
   }
 );
 
@@ -41,13 +43,26 @@ app.get("/about", (req, res) => {
     res.render("about"); // help.pug 찾아서 서버에서 렌더링해라!
 });
 app.post('/contactAdd', (req, res) => { // 등록하려는 문의 정보를 서버로 전송!
-  let type = req.body.type;
+  let type = req.body.type == 1 ? "요청" : "문의";
   let name = req.body.name;
   let phone = req.body.phone;
   let email = req.body.email;
   let title = req.body.title;
   let memo = req.body.memo;
-  console.log(type, name, phone, email, title, memo);
+  // console.log(type, name, phone, email, title, memo);
+  let sql = `INSERT INTO contact.contacts (gubun, name, phone, email, title, memo, regdate)
+  VALUES ('${type}', '${name}', '${phone}', '${email}', '${title}', '${memo}', current_time()) `
+
+  //query 실행명령
+conn.query(
+  sql,
+  function(err, results, fields) {
+    if(err) throw error;
+    console.log('정상적으로 데이터가 입력 되었습니다.');
+    res.send("<script>alert('등록되었습니다'); location.href='/';</script>");
+  }
+);
+
 })
 
 app.listen(port, () => {
